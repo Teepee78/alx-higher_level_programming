1fd8653c138e311046941dc50afcb7f538ea8b24
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-28 16:45:10 +0100

    Update

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,53 +7,49 @@
  * insert_node - inserts a number in a sorted singly linked list
  * @head: singly linked list
  * @number: number to be inserted
+ * Return: pointer to node or NULL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *temp = *head;
-    listint_t *list = *head;
-    int i = 0;
+	listint_t *new_node, *temp = *head, *list = *head;
+	int i = 0;
 
-    if (list == NULL)
-        return (NULL);
+	/* make new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
 
-    /* make new node */
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = number;
-
-    /* traverse head */
-    while (list->next != NULL)
-    {
-        /* check if list->n is less than number */
-        if (list->n < number)
-        {
-            /* store present node */
-            temp = list;
-
-            list = list->next;
-        }
-        else
-        {
-            if (i == 0) /* beginning of list */
-            {
-                new_node->next = list;
-                list = new_node;
-            }
-            else if (list->next == NULL) /* end of list */
-            {
-                new_node->next = NULL;
-                temp->next = new_node;
-            }
-            else
-            {
-                new_node->next = list;
-                temp->next = new_node;
-            }
-            break;
-        }
-        i++;
-    }
-    return (new_node);
+	while (list->next != NULL) /* traverse head */
+	{
+		if (list->n < number) /* check if list->n is less than number */
+		{
+			/* store present node */
+			temp = list;
+			list = list->next;
+		}
+		else
+		{
+			if (list == NULL) /* empty list */
+			{
+				new_node->next = NULL;
+				list = new_node;
+			} else if (i == 0) /* beginning of list */
+			{
+				new_node->next = list;
+				list = new_node;
+			} else if (list->next == NULL) /* end of list */
+			{
+				new_node->next = NULL;
+				temp->next = new_node;
+			} else
+			{
+				new_node->next = list;
+				temp->next = new_node;
+			}
+			break;
+		}
+		i++;
+	}
+	return (new_node);
 }
