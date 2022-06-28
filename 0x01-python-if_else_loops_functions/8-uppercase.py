0d2250706c622d82fcdb0e1d84a3bd8c68c604b9
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-28 13:32:25 +0100

    Update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str = ""
-    toUp = ord('a') - ord('A')
+    toup = ord('a') - ord('A')
     for c in str:
-        # if character is not uppercase
-        if ord(c) < ord('A') or ord(c) > ord('Z'):
-            print("{}".format(chr(ord(c) - toUp)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(chr(ord(c) - toup) if islower(c) else c), end="")
     print()
+
+
+def islower(c):
+    return ord(c) >= ord('a') and ord(c) <= ord('z')
