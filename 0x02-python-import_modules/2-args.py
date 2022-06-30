d402933042e115bc7839cf76c8753c3a4c9327b8
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-30 17:34:50 +0100

    Update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv)
+
+    if argc == 1:
+        """0 arguments"""
+        print("0 arguments.")
+    elif argc == 2:
+        """1 argument"""
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    else:
+        """Multiple arguments"""
+        print(f"{argc:d} arguments:")
+        i = 1
+        while i < argc:
+            print(f"{i:d}: {argv[i]:s}")
+            i += 1
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+0. 0-add.py imports a function from a file and prints the result of an addition
+1. 1-calculation.py  imports functions, does some maths and prints the result
+2. 2-args.py prints the number of and the list of arguments
