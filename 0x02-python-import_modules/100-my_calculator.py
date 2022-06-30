724da3fe532971b9a27ff688b2c3da39436e31cc
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-30 19:50:03 +0100

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-from sys import argv, exit
-
-argc = len(argv) - 1
-
-if argc != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-else:
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-
 if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    argc = len(argv) - 1
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        operator = argv[2]
+        b = int(argv[3])
+        
     # Check operator and call relative function
     if operator == "+":
         result = add(a, b)
