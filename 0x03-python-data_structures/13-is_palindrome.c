76b3dbb965c0a49f2c67fec2cd72bbd25b818cf3
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-02 13:52:00 +0100

    Revert

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -17,6 +17,7 @@ listint_t *add_node_beginning(listint_t **head, int n)
 	if (new_node == NULL)
 		exit(98);
 	new_node->n = n;
+
 	new_node->next = *head;
 	*head = new_node;
 
@@ -28,16 +29,10 @@ listint_t *add_node_beginning(listint_t **head, int n)
  * @head: singly linked list
  * Return: reversed list
  */
-listint_t *reverse_list(listint_t **head, int length)
+listint_t *reverse_list(listint_t **head)
 {
 	listint_t *reversed, *singly_list = *head;
 
-	/* go to length element */
-	while (length > 0)
-	{
-		length--;
-		singly_list = singly_list->next;
-	}
 	/* create first node */
 	reversed = malloc(sizeof(listint_t));
 	if (reversed == NULL)
@@ -52,32 +47,11 @@ listint_t *reverse_list(listint_t **head, int length)
 		reversed = add_node_beginning(&reversed, singly_list->n);
 		singly_list = singly_list->next;
 	}
+
 	/* add last element */
 	reversed = add_node_beginning(&reversed, singly_list->n);
 
-	printf("Printing Reversed\n");
-	print_listint(reversed);
-
 	return (reversed);
-}
-
-/**
- * length_list - get length of list
- * @head: singly linked list
- * Return: length of list
- */
-int length_list(listint_t *head)
-{
-	int length = 0;
-
-	while (head->next != NULL)
-	{
-		length++;
-		head = head->next;
-	}
-	length++;
-
-	return (length);
 }
 
 /**
@@ -88,27 +62,13 @@ int length_list(listint_t *head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *reversed, *temp, *singly_list = *head;
-	int length;
 
 	/* check if list is empty */
 	if (*head == NULL || head == NULL || (*head)->next == NULL)
 		return (1);
 
-	/* get length of list */
-	length = length_list(*head) / 2;
-	/* length is 3, compare first and last elements */
-	if (length == 3)
-	{
-		while (singly_list->next != NULL)
-			singly_list = singly_list->next;
-		if (singly_list->n == (*head)->n)
-			return (1);
-		else
-			return (0);
-	}
-
 	/* reverse list */
-	reversed = reverse_list(head, length);
+	reversed = reverse_list(head);
 	temp = reversed;
 
 	/* iterate through both lists and compare the values */
