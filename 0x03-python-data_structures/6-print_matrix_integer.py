d3c33592c55d9943010f07b2b882d9280172a247
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-02 11:32:21 +0100

    Update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,6 +10,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             if i.index(j) == (len(i) - 1):
                 # if item is the last element
-                print("{:d}$".format(j))
+                print("{:d}".format(j))
             else:
                 print("{:d}".format(j), end=" ")
