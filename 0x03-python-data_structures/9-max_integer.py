de5996c14e7d9eab5e906ef3c2f88a7393d4c025
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-02 11:56:54 +0100

    Update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -12,8 +12,9 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    max = 0
     for i in my_list:
+        if my_list.index(i) == 0:
+            max = i
         if i > max:
             max = i
 
