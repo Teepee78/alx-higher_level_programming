5e6f2e47ca8939916bdcddbcac41185bf3fefd45
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-06 14:34:44 +0100

    Update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -10,12 +10,19 @@ def weight_average(my_list=[]):
             denominator = tuple[-1]
         else:
             denominator += tuple[-1]
+
         mult = 1
         for i in tuple:
             mult *= i
+
         if numerator is None:
             numerator = mult
         else:
             numerator += mult
 
     return numerator / denominator
+
+my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+result = weight_average(my_list)
+print("Average: {:0.2f}".format(result))
