b9acab57d3e925635b1dec3737d2455f26df1255
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-06 13:02:40 +0100

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-
-    keys = [key for key in a_dictionary]
-
-    for i, key in enumerate(keys):
-        if i == 0:
-            bestScore = key
-        else:
-            if a_dictionary[key] > a_dictionary[bestScore]:
-                bestScore = key
-    return bestScore
+    return max(my_dict) if my_dict else None
