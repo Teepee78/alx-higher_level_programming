3b1155602ec33e107f4af05cd6e36f32cd61ed59
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-19 13:12:11 +0100

    Update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,12 @@
 """Creates a square
 
     Attributes:
-        Square (class): a class that creates a square"""
+        Square (class): a class that creates a square
+"""
 
 
 class Square:
     """A Square class with validated private instance attribute size"""
-
 
     def __init__(self, size):
         """Initialize a square
