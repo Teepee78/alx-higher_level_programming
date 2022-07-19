68735d87c8259f8d270602696fdc4c2d29cbab82
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-19 13:19:32 +0100

    Update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Creates a square"""
+"""Creates a square
+
+    Attributes:
+        Square (class): a class that creates a square
+"""
 
 
 class Square:
-    """A Square"""
+    """A Square class with validated private instance attribute size
+
+    Args:
+        size (int): size of square
+    """
 
     def __init__(self, size=0):
-        """Initialize a square
-
-        Args:
-            size: size of square
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
