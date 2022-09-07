a1beac0b619d724cf8e66986aa553537c6c1bbe3
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-09-07 23:59:09 +0100

    Update

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,12 @@ SELECT id, name FROM cities
 WHERE state_id = (
     SELECT id FROM states
     WHERE name = California
-);
+)
+ORDER BY id;
+
+-- SELECT id, name
+-- FROM cities
+-- WHERE state_id = (
+--     SELECT id FROM states
+--     WHERE name = 'California')
+-- ORDER BY id;
