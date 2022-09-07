ed8619743e306ae89a5e6362b2e6570ba5df039f
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-09-08 00:11:44 +0100

    Update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,12 +1,10 @@
 -- Creates the database hbtn_0d_usa and the table states
--- CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---
--- USE hbtn_0d_usa;
---
--- CREATE TABLE IF NOT EXISTS states (
---     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
---     name VARCHAR(256) NOT NULL,
--- );
-
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256));
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+);
+
+-- CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256));
