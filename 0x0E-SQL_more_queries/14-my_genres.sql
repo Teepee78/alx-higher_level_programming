221bd257c713f390f3ca6aa57db2bf6749948d0c
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-09-07 23:44:06 +0100

    Update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,8 @@
 -- Lists all genres of the show Dexter
-SELECT `tv_genres.name`
-FROM `tv_genres`
-INNER JOIN `tv_show_genres` AS 'm'
-ON `tv_genres.id` = `m.genre_id`
-INNER JOIN `tv_shows` AS 's'
-ON `m.show_id` = `s.id`
-WHERE `s.title` = 'Dexter'
-ORDER BY `tv_genres.name` ASC;
+SELECT tv_genres.name
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title='Dexter'
+ORDER BY tv_genres.name;
