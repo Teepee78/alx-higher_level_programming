49354c73f1685b72f9b9c6f09b968e9409ba43f7
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-10-03 15:28:06 +0100

    Update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,13 +5,6 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  /* copy array and convert to number */
-  const newArr = [];
-
-  for (let i = 2, j = 0; j < args.length - 2; i++, j++) {
-    newArr[j] = parseInt(args[i]);
-  }
-
-  newArr.sort().reverse();
-  console.log(newArr[1]);
+  args.sort().reverse();
+  console.log(args);
 }
