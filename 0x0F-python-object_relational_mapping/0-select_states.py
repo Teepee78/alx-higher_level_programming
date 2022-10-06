a6e831e2843016d72620981c893c8908a882b5a7
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-10-06 10:19:35 +0100

    Update

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module lists all states from hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+# credentials
+username = argv[1]
+password = argv[2]
+database = argv[3]
+
+db = MySQLdb.connect(
+    host='0.0.0.0:3306',
+    user=username,
+    passwd=password,
+    db=database
+)
+
+cursor = db.cursor()
+
+rows = cursor.execute("SELECT * FROM hbtn_0e_0_usa")
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,5 @@
+# 0x0F Python - Object relational mapping
+
+### 0-select_states.py
+
+	Lists all states from the database hbtn_0e_0-usa
