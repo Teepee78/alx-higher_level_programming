827ff9b5c86ab54336169f8a9736a598afdbf569
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-15 12:54:20 +0100

    Update

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// prints the number of movies where the character "Wedge Antilles" is present
+const request = require('request');
+
+const url = process.argv[2];
+const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+let movies = 0;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  JSON.parse(body).results.forEach(element => {
+    element.characters.forEach(character => {
+      // check for character
+      if (character === characterUrl) {
+        movies++;
+      }
+    });
+  });
+  console.log(movies);
+});
